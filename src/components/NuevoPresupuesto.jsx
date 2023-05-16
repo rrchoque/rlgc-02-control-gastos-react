e767783f367a5e79152e86fce1b618c03eba6eed
rrchoque <reynaldorodrigochoque@gmail.com>
2023-05-16 00:27:50 -0400

    Se validó registro de presupuesto inicial, y se comenzó con el control de presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto,setIsValidPresupuesto }) => {
+
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e)=> {
+    e.preventDefault();
+    
+    if(!presupuesto || presupuesto < 0) {
+      setMensaje('No es un presupuesto válido')
+      return
+    }
+    
+    setMensaje('')
+    setIsValidPresupuesto(true)
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-      <form className='formulario'>
+      <form className='formulario' onSubmit={handlePresupuesto}>
         <div className='campo'>
             <label>Definir Presupuesto</label>
 
             <input 
                 className='nuevo-presupuesto'
-                type="text" 
+                type="number" 
                 placeholder='Añade tu Presupuesto'
                 value={presupuesto}
-                onChange={(e) => setPresupuesto(e.target.value)}
+                onChange={(e) => setPresupuesto(Number(e.target.value))}
             />
-
-            <input type="submit" value="Añadir" />
         </div>
+        
+        <input type="submit" value="Añadir" />
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje> }
       </form>
     </div>
   )
